dd9d9bad0f1942ae3409f65d19bb9dcb71041898
rippedpleasure <luvyou2day@gmail.com>
2020-11-07 17:07:00 +0500

    Digits -> letters in rows and columns. Some comments

--- battleship.py
+++ battleship.py
@@ -1,60 +1,89 @@
 """
-Игра «Морской бой» на поле 6x6.
+Игра «Морской бой» для произвольного размера поля и наборов кораблей.
 Итоговое практическое задание B7.5 для SkillFactory.
+Поддерживается режим игры только с компьютерным оппонентом.
+Формат ввода координат: «строка колонка». Можно использовать любой
+небуквенный разделитель, или вводить координаты слитно.
+
 Используются модули только из стандартной библиотеки Python 3.8.5.
 """
+# !!! КОММЕНТАРИЙ ДЛЯ МЕНТОРА !!!
+#
+# 1. Решил немного изменить внешний вид поля по сравнению с образцом.
+#    На мой взгляд, символы O-X-T слишком сливаются друг с другом
+#    из-за одинаковых размеров, и иногда сложно понять, что происходит
+#    на поле. Все символы настраиваются в константном блоке.
+# 2. Дополнил правила игры в соответствии с настольной версией игры,
+#    по крайней мере, с той, с которой я знаком. При потоплений вражеского
+#    корабля сообщается, что он именно убит, а не просто ранен, и,
+#    как следствие, все клетки вокруг него помечаются как гарантированно
+#    пустые. После этого компьютер со своим примитивным алгоритмом стал
+#    чуть лучше играть.
+# 3. В теории поддерживаются поля и списки кораблей большего размера
+#    (изменить можно также в константном блоке). С учётом полей размером
+#    больше девяти клеток решено было использовать буквы вместо цифр
+#    для обозначения рядов и колонок.
+# 4. Вместо функции print_intro изначально использовался метод draw
+#    класса Board, но для более симпатичного (параллельного) вывода
+#    игровых полей решил использовать эту функцию. Конечно, в метод draw
+#    одной доски можно было бы передавать аргументом вторую, но не думаю,
+#    что это очень изящный способ.
+#    Изначальный код метода оставлен в программе и закомментирован.
 
 
 import os
 import random
 import time
+from string import ascii_uppercase as letters
 
 
-CLEAR_SCREEN = 'cls' if os.name == 'nt' else 'clear'
-WIDTH = 50
-TIMEOUT = 1
+BOARD_SIZE = 6
+SHIP_RULES = [3, 2, 2, 1, 1, 1, 1]
 EMPTY_SYMBOL = '~'
 SHIP_SYMBOL = '■'
 HIT_SYMBOL = 'X'
 MISS_SYMBOL = '·'
-BOARD_SIZE = 6
-SHIP_RULES = [3, 2, 2, 1, 1, 1, 1]
+SCR_WIDTH = BOARD_SIZE * 8
+CLEAR_SCREEN = 'cls' if os.name == 'nt' else 'clear'
+TIMEOUT = 1
 
 
-def print_intro(board1, board2, with_ships=True):
+def print_intro(board1, board2, with_ships=False):
     """
     Функция очистки экрана и вывода игровых полей.
     Аргументы:
     with_ships — отображать или нет при выводе поля противника расставленные
     корабли. По умолчанию False.
     """
-    # Изначально использовался метод draw класса Board, но для более
-    # симпатичного вывода решил использовать эту функцию.
-    # Код метода оставлен в программе и закомментирован.
     os.system(CLEAR_SCREEN)
 
-    print('-' * WIDTH)
-    print('Морской бой'.center(WIDTH))
+    print('-' * SCR_WIDTH)
+    print('Морской бой'.center(SCR_WIDTH))
     print()
-    print('формат ввода ходов: «строка колонка»'.center(WIDTH))
-    print('-' * WIDTH)
+    print('формат ввода ходов: «строка колонка»'.center(SCR_WIDTH))
+    print('-' * SCR_WIDTH)
     print()
-    print('Игрок'.center(WIDTH // 2) + 'Компьютер'.center(WIDTH // 2))
-    print(f'Кораблей: {len(board1.ships)}'.center(WIDTH // 2)
-        + f'Кораблей: {len(board2.ships)}'.center(WIDTH // 2))
+    print('Игрок'.center(SCR_WIDTH // 2) + 'Компьютер'.center(SCR_WIDTH // 2))
+    print(f'Кораблей: {len(board1.ships)}'.center(SCR_WIDTH // 2)
+        + f'Кораблей: {len(board2.ships)}'.center(SCR_WIDTH // 2))
     print()
 
-    col_numbers1 = ' ' + '|'.join(map(str, range(1, board1.size + 1)))
-    col_numbers2 = ' ' + '|'.join(map(str, range(1, board2.size + 1)))
-    print(col_numbers1.center(WIDTH // 2) + col_numbers2.center(WIDTH // 2))
-    for row_number, rows in enumerate(zip(board1.state, board2.state), 1):
+    col_numbers1 = ' ' + '|'.join(letters[:board1.size])
+    col_numbers2 = ' ' + '|'.join(letters[:board2.size])
+    print(col_numbers1.center(SCR_WIDTH // 2)
+        + col_numbers2.center(SCR_WIDTH // 2))
+
+    for row_number, rows in enumerate(zip(board1.state, board2.state)):
         hidden_row = [EMPTY_SYMBOL if cell == SHIP_SYMBOL
                       else cell for cell in rows[1]]
-        row1 = str(row_number) + '|' + '|'.join(map(str, rows[0]))
-        row2 = (str(row_number) + '|' + '|'.join(map(str, rows[1]))
+        row1 = letters[row_number] + '|' + '|'.join(map(str, rows[0]))
+        row2 = (letters[row_number] + '|' + '|'.join(map(str, rows[1]))
                 if with_ships else
-                str(row_number) + '|' + '|'.join(map(str, hidden_row)))
-        print(row1.center(WIDTH // 2) + row2.center(WIDTH // 2))
+                letters[row_number] + '|' + '|'.join(map(str, hidden_row)))
+        print(row1.center(SCR_WIDTH // 2) + row2.center(SCR_WIDTH // 2))
+
+    print()
+    print('-' * SCR_WIDTH)
     print()
 
 
@@ -105,10 +134,12 @@ class Board:
         dummy = Board()
         while len(self.ships) < len(self.ship_rules):
             ship_size = self.ship_rules[len(self.ships)]
+
+            # Автоматическая расстановка.
             if auto:
                 # На случай возникновения тупиковой ситуации, при которой
                 # следующий корабль невозможно разместить, используется
-                # ограниченное количество попыток (20). По их истечению, поле
+                # ограниченное количество попыток (20). По их истечении поле
                 # сбрасывается, и расстановка начинается сначала.
                 try_count = 0
                 while try_count <= 20:
@@ -122,29 +153,36 @@ class Board:
                         break
                 if try_count > 20:
                     self.reset()
+
+            # Ручная расстановка.
             else:
                 print_intro(self, dummy)
                 print(f'Расстановка — Корабль №{len(self.ships) + 1}, '
                       f'{ship_size}-палубный')
-                if ship_size > 1:
-                    orientation = input('Введите ориентацию корабля — '
-                        'горизонтальная (h) или вертикальная (v): ')
-                    start_position = map(lambda x: x - 1,
-                        map(int, input('Введите координаты верхней левой '
-                                       'точки корабля: ').split()))
-                else:
-                    orientation = 'h'
-                    start_position = map(lambda x: x - 1,
-                        map(int, input('Введите координаты корабля: ').split()))
-                ship = Ship(ship_size, orientation, start_position)
-                if self.is_ship_fit(ship):
-                    self.add_ship(ship)
-                else:
+                try:
+                    if ship_size > 1:
+                        orientation = input('Введите ориентацию корабля — '
+                            'горизонтальная (h) или вертикальная (v): ')
+                        start_position = (letters.index(l) for l in
+                        input('Введите координаты верхней левой '
+                        'точки: ').upper() if l in letters)
+                    # Если корабль одноклеточный, то ориентация неважна.
+                    else:
+                        orientation = 'h'
+                        start_position = (letters.index(l) for l in
+                            input('Введите координаты корабля: ').upper()
+                            if l in letters)
+                    ship = Ship(ship_size, orientation, start_position)
+                    if self.is_ship_fit(ship):
+                        self.add_ship(ship)
+                    else:
+                        raise ValueError
+                except ValueError:
                     print('Корабль нельзя разместить в указанных координатах.')
-                    reset = input('Введите «(r)eset», чтобы начать '
-                                  'расстановку сначала.\n'
-                                  'Введите «(a)uto», чтобы закончить '
-                                  'расстановку автоматически: ')
+                    print('(r)eset — начать расстановку сначала.\n'
+                          '(a)uto — закончить расстановку автоматически.\n'
+                          '<Enter> для продолжения:', end=' ')
+                    reset = input()
                     if reset.lower() in ('r', 'reset'):
                         self.reset()
                     elif reset.lower() in ('a', 'auto'):
@@ -187,24 +225,25 @@ class Board:
         # Если обе проверки пройдены, значит, корабль можно разместить.
         return True
 
-    def take_shot(self, ai):
+    def take_shot(self, is_ai):
         """
-        Метод проведения выстрела по указанным координатам. Возвращает True при
-        попадании и False при промахе.
+        Метод проведения выстрела по указанным координатам.
+        Возвращает True при попадании и False при промахе.
         Аргументы:
-        ai — кто делает выстрел: компьютер или человек.
+        is_ai — кто делает выстрел: компьютер или человек.
         """
         while True:
-            if ai:
+            if is_ai:
                 x, y = (random.randrange(self.size),
                         random.randrange(self.size))
                 if self.state[x][y] in (MISS_SYMBOL, HIT_SYMBOL):
                     continue
                 time.sleep(TIMEOUT)
-                print(*map(lambda x: x + 1, (x, y)))
+                print(letters[x], letters[y])
                 break
             try:
-                x, y = map(lambda x: x - 1, map(int, input().split()))
+                x, y = (letters.index(l)
+                        for l in input().upper() if l in letters)
                 if x < 0 or x >= self.size or y < 0 or y >= self.size:
                     raise IndexError('Таких координат не существует.')
                 if self.state[x][y] in (MISS_SYMBOL, HIT_SYMBOL):
@@ -250,7 +289,8 @@ class Board:
 
     def mark_ship_dead(self, shot_x, shot_y):
         """
-        Метод, помечающий символом выстрела все ячейки вокруг убитого корабля.
+        Метод, помечающий символом выстрела все ячейки вокруг убитого корабля,
+        и удаляющий убитый корабль из списка кораблей.
         Аргументы:
         shot_x, shot_y — координаты точки выстрела, по которой определяется,
         в какой именно корабль совершён выстрел.
@@ -320,7 +360,7 @@ def battleship():
 
         # Если текущий игрок — компьютер, то ход происходит автоматически,
         # и если выстрел попал, то не меняем текущего игрока.
-        if not current_board.take_shot(ai=current_board == board1):
+        if not current_board.take_shot(is_ai=current_board == board1):
             current_board = board2 if current_board == board1 else board1
         if current_board.is_lose():
             break
